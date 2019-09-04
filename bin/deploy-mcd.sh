8b296d16ff537952e0cecc9444c0238f026f9640
Ed Noepel <ed@makerdao.com>
2019-09-04 14:30:22 -0400

    Updated scripts to accommodate dss-deploy for 0.2.12

--- bin/deploy-mcd.sh
+++ bin/deploy-mcd.sh
@@ -2,11 +2,13 @@
 
 . /home/maker/.nix-profile/etc/profile.d/nix.sh
 
-cd src/deployment-scripts
+pushd src/deployment-scripts
 
 export TESTNET_HOST="parity"
 export KEYSTORE_PATH=/testchain-data/keys/DevelopmentChain
 export ETH_FROM=0x00a329c0648769A73afAc7F9381E08FB43dBEA72
 
 . lib/setup-env.sh
-./deploy-testchain.sh
+nix run -f /home/maker/src/deployment-scripts -c deploy-testchain.sh
+
+popd
