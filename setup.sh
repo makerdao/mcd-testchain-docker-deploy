646f74ee5f2b65cedb528eac34c3d690c7512277
Ed Noepel <46749157+EdNoepel@users.noreply.github.com>
2019-09-09 11:32:42 -0400

    update to accommodate dss-deploy 0.2.12 (#3)
    * Merged all changes from my fork, which github didn't believe was a fork
    
    * Integration test with larger flap/flop lot sizes, and shorter bid durations.
    
    * Add context for building mcd-test image
    
    * Updated scripts to accommodate dss-deploy for 0.2.12
    
    * Updated configs for dss 0.2.12

--- setup.sh
+++ setup.sh
@@ -3,6 +3,6 @@
 git clone git@github.com:makerdao/dss-deploy-scripts.git src/deployment-scripts
 pushd src/deployment-scripts
 git fetch --all --tags --prune
-git checkout tags/0.2.10
+git checkout tags/0.2.12
 git submodule deinit -f --all && dapp update
 git submodule update --init --recursive
