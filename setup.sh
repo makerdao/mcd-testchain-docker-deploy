9230c8134384825833328ee5c95feb9c555ed59a
Ed Noepel <ed@makerdao.com>
2019-10-28 11:23:56 -0400

    Updated for dss 0.2.15

--- setup.sh
+++ setup.sh
@@ -5,6 +5,6 @@
 git clone git@github.com:makerdao/dss-deploy-scripts.git src/deployment-scripts
 pushd src/deployment-scripts
 git fetch --all --tags --prune
-git checkout tags/0.2.14
+git checkout tags/0.2.15
 git submodule deinit -f --all && dapp update
 git submodule update --init --recursive
