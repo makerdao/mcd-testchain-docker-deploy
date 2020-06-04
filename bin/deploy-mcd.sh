89936681c6c4ff5ce06ac83f798ec0b105ab26cc
Ed Noepel <ed@makerdao.com>
2020-06-04 13:38:37 -0400

    updated 1.0.7

--- bin/deploy-mcd.sh
+++ bin/deploy-mcd.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+set -x
+
 . /home/maker/.nix-profile/etc/profile.d/nix.sh
 
 pushd src/deployment-scripts
@@ -9,6 +11,6 @@ export KEYSTORE_PATH=/testchain-data/keys/DevelopmentChain
 export ETH_FROM=0x00a329c0648769A73afAc7F9381E08FB43dBEA72
 
 . lib/setup-env.sh
-nix run -f /home/maker/src/deployment-scripts -c dss-deploy testchain
+nix run -v -f /home/maker/src/deployment-scripts -c dss-deploy testchain
 
 popd
