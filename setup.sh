c3c943a5023cdfe674a2fa3ec554e107f8d7f3bf
grandizzy <38490174+grandizzy@users.noreply.github.com>
2020-09-16 12:58:28 +0300

    Update setup.sh
    clone dss-deploy-scripts as https

--- setup.sh
+++ setup.sh
@@ -2,7 +2,7 @@
 
 # see also: https://gist.github.com/godsflaw/c507c88cdc2359e8bda8cc3b34b112d7
 
-git clone git@github.com:makerdao/dss-deploy-scripts.git src/deployment-scripts
+git clone https://github.com/makerdao/dss-deploy-scripts.git src/deployment-scripts
 pushd src/deployment-scripts
 git fetch --all --tags --prune
 git checkout master
